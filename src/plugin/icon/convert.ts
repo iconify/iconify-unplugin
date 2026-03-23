73dd4c0bde9bc85b35458f68ba0af271f3e3db5a
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-03-23 13:56:25 +0200

    chore: update dependencies

--- src/plugin/icon/convert.ts
+++ src/plugin/icon/convert.ts
@@ -2,7 +2,7 @@ import type { IconifyIcon } from '@iconify/types';
 import type { LoadedIconData } from '../types/icon.js';
 import { convertIconifyIconToFactoryContent } from '@cyberalien/svg-utils/lib/components/prepare/iconify.js';
 import type { ConvertSVGContentOptions } from '@cyberalien/svg-utils/lib/svg-css/types.js';
-import { normaliseIconifyIcon } from '@cyberalien/svg-utils/lib/iconify/icon/nornalise.js';
+import { normaliseIconifyIcon } from '@cyberalien/svg-utils/lib/iconify/icon/normalise.js';
 import type { IconMode } from '../types/mode.js';
 import { defaultCSSHashOptions } from '../helpers/config.js';
 
